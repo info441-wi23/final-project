4de4f025833964bed720a7043465b132eda623ad
davidngo123 <ngodavid215@gmail.com>
2023-03-01 00:24:33 -0800

    added in GET location react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useState,  useEffect } from "react";
 import './App.css';
 import HomePage from './pages/homepage';
 
-const fakeCardData = [
-  { name: 'Test', avgRating: '4.5' },
-  { name: 'Card', avgRating: '4.5' },
-  { name: 'Using', avgRating: '4.5' },
-  { name: 'Fake', avgRating: '4.5' },
-  { name: 'Data', avgRating: '4.5' },
-  { name: 'Test', avgRating: '2.0'},
-  { name: "test", avgRating: "2.5"},
-  { name: "3000", avgRating: "1.3"}
-]
+
+
+
 
 function App() {
+const fakeCardData = []
+const [data, setData] = useState({});
+  // pass id to fetch relevant data
+  const url = 'http://localhost:8080/studyspots'
+  useEffect(() => {
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => {
+        // save data from fetch request to state
+        setData(r);
+      });
+  }, [url]);
+  console.log(data)
+  for (var i = 0; i < data.length; i++) {
+    fakeCardData.push({name: data[i].name, avgRating: data[i].avgRating})
+  }
+
+  console.log(data)
+
+
   return (
     <div className="App">
       <HomePage cards={fakeCardData} />
