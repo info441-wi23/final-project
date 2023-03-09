53d34663fe6fb225ba74d922568821f400cf7d6d
davidngo123 <ngodavid215@gmail.com>
2023-03-09 01:45:46 -0800

    finished GET for study spot

--- rate-my-studyspot/app.js
+++ rate-my-studyspot/app.js
@@ -6,6 +6,9 @@ import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import msIdExpress from "microsoft-identity-express";
 import sessions from "express-session";
+import studySpotsRouter from "../javascripts/routes/controllers/studyspots.js"
+import reviewsRouter from '../javascripts/routes/controllers/review.js'
+import userRouter from "../javascripts/routes/controllers/users.js"
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -21,6 +24,54 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const appSettings = {
+    appCredentials: {
+        clientId: "f2f9caa8-2681-48b8-9675-3110aaaf9e34",
+        tenantId: "f6b6dd5b-f02f-441a-99a0-162ac5060bd2",
+        clientSecret: "ytD8Q~QHH0bCc9c6WzhGcdW84MnvKqKf5AMrFc3x"
+    },
+    authRoutes: {
+        redirect: "http://localhost:8080/redirect",
+        error: "/error",
+        unauthorized: "/unauthorized"
+    }
+};
+const oneDay = 1000 * 60 * 60 * 24
+app.use(sessions({
+    secret: "37686540-bf0a-4c1c-9b40-3bdf3f0d7eaf",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}))
+
+const msid = new msIdExpress.WebAppAuthClientBuilder(appSettings).build()
+app.use(msid.initialize())
+
+/*
+app.get("/", (req, res) => {
+    console.log(req.session.account.name);
+    console.log("app.get(/) called")
+    res.redirect("http://localhost:3000")
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.send("hello");
+});
+*/
+
+app.get('/signin',
+    msid.signIn({ postLoginRedirect: '/' })
+)
+
+app.get('/signout',
+    msid.signOut({ postLogoutRedirect: '/' })
+)
+
+app.get('/error', (req, res) => {
+    res.status(500).send("Error: Server error")
+})
+
+app.get('/unauthorized', (req, res) => {
+    res.status(401).send("Error: Unauthorized")
+})
 
 app.use((req, res, next) => {
     req.models = models;
@@ -32,6 +83,10 @@ app.get("/", (req, res) => {
     console.log(models);
     res.sendFile(__dirname + "/index.html");
 })
+app.use("/studyspots", studySpotsRouter)
+app.use('/reviews', reviewsRouter)
+app.use("/user", userRouter)
+
 
 app.listen(3000);
 
--- rate-my-studyspot/public/javascripts/index.js
+++ rate-my-studyspot/public/javascripts/index.js
@@ -5,3 +5,66 @@ function handleAddLocation() {
     );
     console.log(callToAction)
 }
+
+function hideForm() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+async function init(){
+    loadPosts();
+}
+
+async function fetchJSON(route, options){
+    let response
+    try{
+        response = await fetch(route, {
+            method: options && options.method ? options.method : "GET",
+            body: options && options.body ? JSON.stringify(options.body) : undefined,
+            headers: options && options.body ? {'Content-Type': 'application/json'}: undefined
+        })
+    }catch(error){
+        displayError()
+        throw new Error(
+            `Error fetching ${route} with options: ${options ? JSON.stringify(options) : options}
+             No response from server (failed to fetch)`)
+    }
+    let responseJson;
+    try{
+        responseJson = await response.json();
+    }catch(error){
+        let responseText = await response.text();
+        displayError()
+        throw new Error(
+            `Error fetching ${route} with options: ${options ? JSON.stringify(options) : options}
+            Status: ${response.status}
+            Response wasn't json: ${responseText ? JSON.stringify(responseText) : responseText}`)
+    }
+    if(response.status < 200 || response.status >= 300 || responseJson.status == "error"){
+        displayError()
+        throw new Error(
+            `Error fetching ${route} with options: ${options ? JSON.stringify(options) : options}
+            Status: ${response.status}
+            Response: ${responseJson ? JSON.stringify(responseJson) : responseJson}`)
+    }
+    return responseJson
+}
+async function loadPosts(){
+    document.getElementById("posts_box").innerText = "Loading...";
+    let spotsJson = await fetchJSON(`/studyspots`)
+    
+    let postsHtml = spotsJson.map(spotsInfo => {
+        return `
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src='https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png' 
+        alt='study spot location placeholder'>
+        <div class="card-body">
+        <p class="card-title">${spotsInfo.name}</p>
+        </div>
+    </div>`
+    }).join("\n");
+    document.getElementById("posts_box").innerHTML = postsHtml;
+}
