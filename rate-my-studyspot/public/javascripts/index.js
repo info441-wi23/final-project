21b0c27b3f9826a408f75f247489cc92e9b5f7c0
davidngo123 <ngodavid215@gmail.com>
2023-03-09 02:15:13 -0800

    added in azure identity

--- rate-my-studyspot/public/javascripts/index.js
+++ rate-my-studyspot/public/javascripts/index.js
@@ -15,43 +15,11 @@ function hideForm() {
     }
   }
 async function init(){
+    await loadIdentity();
     loadPosts();
 }
 
-async function fetchJSON(route, options){
-    let response
-    try{
-        response = await fetch(route, {
-            method: options && options.method ? options.method : "GET",
-            body: options && options.body ? JSON.stringify(options.body) : undefined,
-            headers: options && options.body ? {'Content-Type': 'application/json'}: undefined
-        })
-    }catch(error){
-        displayError()
-        throw new Error(
-            `Error fetching ${route} with options: ${options ? JSON.stringify(options) : options}
-             No response from server (failed to fetch)`)
-    }
-    let responseJson;
-    try{
-        responseJson = await response.json();
-    }catch(error){
-        let responseText = await response.text();
-        displayError()
-        throw new Error(
-            `Error fetching ${route} with options: ${options ? JSON.stringify(options) : options}
-            Status: ${response.status}
-            Response wasn't json: ${responseText ? JSON.stringify(responseText) : responseText}`)
-    }
-    if(response.status < 200 || response.status >= 300 || responseJson.status == "error"){
-        displayError()
-        throw new Error(
-            `Error fetching ${route} with options: ${options ? JSON.stringify(options) : options}
-            Status: ${response.status}
-            Response: ${responseJson ? JSON.stringify(responseJson) : responseJson}`)
-    }
-    return responseJson
-}
+
 async function loadPosts(){
     document.getElementById("posts_box").innerText = "Loading...";
     let spotsJson = await fetchJSON(`/studyspots`)
@@ -62,7 +30,8 @@ async function loadPosts(){
         <img class="card-img-top" src='https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png' 
         alt='study spot location placeholder'>
         <div class="card-body">
-        <p class="card-title">${spotsInfo.name}</p>
+        <h5 class="card-title">${spotsInfo.name}</h5>
+        <p>Rating: /5</p>
         </div>
     </div>`
     }).join("\n");
