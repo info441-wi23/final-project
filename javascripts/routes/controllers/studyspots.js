de721dd0586b003100a3d15aad6f5fb188259345
davidngo123 <ngodavid215@gmail.com>
2023-03-09 09:39:22 -0800

    studyspots

--- javascripts/routes/controllers/studyspots.js
+++ javascripts/routes/controllers/studyspots.js
@@ -12,7 +12,23 @@ router.get('/', async (req, res) => {
     }
     try {
         let allStudySpots = await req.models.StudySpot.find();
-        res.send(allStudySpots);
+        let spots = [];
+        for (let i = 0; i < allStudySpots.length; i++) {
+          const reviews = await req.models.Review.find({'studyspot': allStudySpots[i]._id })
+          let avg = 0;
+          if(reviews.length != 0){
+               for(let j = 0; j < reviews.length; j++){
+                    avg = avg + reviews[j].rating;
+               }
+               avg = avg/reviews.length;
+          }
+            spots.push({
+                "name": allStudySpots[i].name,
+                "spotid": allStudySpots[i]._id,
+                "rating": avg,
+            })
+        }
+        res.send(spots);
     } catch (error) {
         console.log("Error: " + error);
         res.status(500).json({ "status": "error", "error": error });
@@ -32,31 +48,35 @@ router.get('/getOne', async (req, res) => {
 
 // create a new studyspot LOCATION 
 router.post('/', async (req, res) => {
+     console.log(req.body)
     try {
         if (req.session.isAuthenticated) {
-               
+          var today = new Date();
+          today.setHours(0, 0, 0, 0);
             let allPosts = await req.models.StudySpot.find({ 'name': req.body.name })
             if (allPosts.length == 0) {
                 const newLocation = new req.models.StudySpot({
                     name: req.body.name,
                     address: req.body.address,
-                    initialRating: req.body.initialRating,
-                    dateCreated: req.body.dateCreated
+                    dateCreated: today
                 })
                 await newLocation.save();
+                let num = parseInt(req.body.rating.charAt(1))
                 const newReview = new req.models.Review({
                     name: req.body.name,
                     author: req.session.account.username,
                     studyspot: newLocation._id,
-                    rating: req.body.initialRating,
-                    reviewText: req.body.authorReview,
-                    dateCreated: req.body.dateCreated
+                    rating: num,
+                    reviewText: req.body.review,
+                    dateCreated: today
                 })
+                
                 await newReview.save()
+                res.json({"status": "success"})
             } else {
                 res.status(402).send('Location already in database')
             }
-            res.status(200).send('posted')
+            res.status(202).send('posted')
         } else {
             res.status(401).send({
                 status: 'error',
